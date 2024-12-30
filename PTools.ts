f1cbd9267a5b59f9738b522f48c2f847b53af74f
RickChen13 <rickchen13@gmail.com>
2024-12-30 15:45:15 +0800

    更明确的类型提示

--- PTools.ts
+++ PTools.ts
@@ -1,6 +1,6 @@
 // 与php功能函数相似的函数
 
-export const in_array = (value: any, array: any) => {
+export const in_array = <T>(value: T, array: T[]) => {
     for (let val in array) {
         if (value == array[val]) {
             return true;
